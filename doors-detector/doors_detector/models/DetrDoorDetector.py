a3c3c8bf008a77eecca4bbeec33a2aea25f386c4
micheleantonazzi <m7242872428>
2021-07-13 11:47:02 +0200

    added DetrDoorDetector

---
+++ doors-detector/doors_detector/models/DetrDoorDetector.py
@@ -0,0 +1,52 @@
+import torch
+from torch import nn
+
+from doors_detector.models.mlp import MLP
+from doors_detector.models.model_names import ModelName
+
+
+class DetrDoorDetector(nn.Module):
+    """
+    This class builds a door detector starting from a detr pretrained module.
+    Basically it loads a dtr module and modify its structure to recognize door.
+    """
+    def __init__(self, model_name: ModelName, pretrained: bool):
+        """
+
+        :param model_name: the name of the detr base model
+        :param pretrained: it refers to the DetrDoorDetector class, not to detr base model.
+                            It True, the DetrDoorDetector's weights are loaded, otherwise the weights are loaded only for the detr base model
+        """
+        super(DetrDoorDetector, self).__init__()
+        self._model_name = model_name
+        self.model = torch.hub.load('facebookresearch/detr', model_name, pretrained=pretrained)
+
+        # Change the last part of the model
+        del self.model.class_embed
+        del self.model.bbox_embed
+
+        self.model.class_embed = nn.Linear(256, 2)
+        self.model.bbox_embed = MLP(256, 256, 4, 3)
+
+
+    def forward(self, x):
+        x = self.model(x)
+
+        """
+        It returns a dict with the following elements:
+               - "pred_logits": the classification logits (including no-object) for all queries.
+                                Shape=[batch_size x num_queries x (num_classes + 1)]
+               - "pred_boxes": The normalized boxes coordinates for all queries, represented as
+                               (center_x, center_y, height, width). These values are normalized in [0, 1],
+                               relative to the size of each individual image (disregarding possible padding).
+                               See PostProcess for information on how to retrieve the unnormalized bounding box.
+               - "aux_outputs": Optional, only returned when auxilary losses are activated. It is a list of
+                                dictionnaries containing the two above keys for each decoder layer.
+        """
+        return x
+
+    def eval(self):
+        self.model.eval()
+
+    def train(self):
+        self.model.train()
--- doors-detector/scripts/train_custom_detr_doors_dataset.py
+++ doors-detector/scripts/train_custom_detr_doors_dataset.py
@@ -1,7 +1,6 @@
-import torch
-from matplotlib import pyplot as plt
-import torchvision.transforms as T
 from doors_detector.dataset.datasets_creator import DatasetsCreator
+from doors_detector.models.DetrDoorDetector import DetrDoorDetector
+from doors_detector.models.model_names import DETR_RESNET50
 
 door_dataset_path = '/home/michele/myfiles/doors_dataset'
 
@@ -11,8 +10,8 @@ if __name__ == '__main__':
     datasets_creator.consider_n_folders(3)
     train, test = datasets_creator.creates_dataset(train_size=0.7, test_size=0.3, split_folder=True, folder_train_ratio=0.8, use_all_samples=True)
 
-    img, target, door_sample = train[1]
-    door_sample.visualize()
+    img, target, door_sample = train[0]
     print(img.size(), target)
 
-    model =
+    model = DetrDoorDetector(model_name=DETR_RESNET50, pretrained=False)
+
