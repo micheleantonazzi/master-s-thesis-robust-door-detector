2794169bfe3f3f287cc40eaae0f406cb40daedd5
micheleantonazzi <m7242872428>
2021-07-15 20:01:32 +0200

    find_doors_using_custom_detr.py: fixed numpy's seed

--- doors-detector/scripts/find_doors_using_custom_detr.py
+++ doors-detector/scripts/find_doors_using_custom_detr.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     np.random.seed(params['seed'])
     random.seed(params['seed'])
 
-    datasets_creator = DatasetsCreator(door_dataset_path, numpy_seed=0)
+    datasets_creator = DatasetsCreator(door_dataset_path)
     datasets_creator.consider_samples_with_label(label=1)
     datasets_creator.consider_n_folders(3)
     train, test = datasets_creator.creates_dataset(train_size=0.7, test_size=0.3, split_folder=False, folder_train_ratio=0.8, use_all_samples=True)
