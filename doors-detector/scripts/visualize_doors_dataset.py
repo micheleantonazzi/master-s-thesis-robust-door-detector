ca3a7d10231ba5d4f5ae1b22f7b86243eb05f29a
micheleantonazzi <m7242872428>
2021-07-19 13:29:53 +0200

    visualize_doors_dataset.py: fixed

--- doors-detector/scripts/visualize_doors_dataset.py
+++ doors-detector/scripts/visualize_doors_dataset.py
@@ -6,17 +6,18 @@ from matplotlib import pyplot as plt
 import torchvision.transforms as T
 from doors_detector.dataset.datasets_creator import DatasetsCreator
 from doors_detector.models.detr import PostProcess
+from gibson_env_utilities.doors_dataset.door_sample import DOOR_LABELS
 
-door_dataset_path = '/home/michele/myfiles/doors_dataset'
+door_dataset_path = '/home/michele/myfiles/doors_dataset_labelled'
 
 
 params = {
     'seed': 0
 }
 
+COLORS = np.array([[0, 0, 255], [0, 255, 0]], dtype=float) / np.array([[255, 255, 255], [255, 255, 255]], dtype=float)
 
-COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
-          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
 
 if __name__ == '__main__':
 
@@ -27,7 +28,7 @@ if __name__ == '__main__':
 
     datasets_creator = DatasetsCreator(door_dataset_path)
     datasets_creator.consider_samples_with_label(label=1)
-    datasets_creator.consider_n_folders(3)
+    datasets_creator.consider_n_folders(1)
     train, test = datasets_creator.creates_dataset(train_size=0.9, test_size=0.1, split_folder=False, folder_train_ratio=0.8, use_all_samples=True)
 
     post_processor = PostProcess()
@@ -39,8 +40,11 @@ if __name__ == '__main__':
 
         boxes = target['boxes']
         outputs = {'pred_boxes': boxes.unsqueeze(0),
-                   'pred_logits': torch.tensor([[[-0.99, 0.99] for _ in range(len(boxes))]], dtype=torch.float32)
+                   'pred_logits': torch.tensor([[[-0.99, -0.2, -0.2] for _ in range(len(boxes))]], dtype=torch.float32)
                    }
+
+        for i, label in enumerate(target['labels']):
+            outputs['pred_logits'][0][i][label] = 0.7
 
         processed = post_processor(outputs=outputs, target_sizes=torch.tensor([img.size()[1:]]))[0]
 
@@ -55,12 +59,11 @@ if __name__ == '__main__':
 
         plt.imshow(T.ToPILImage(mode='RGB')(pil_image).convert("RGB"))
         ax = plt.gca()
-
-        colors = COLORS * 100
-        for label, score, (xmin, ymin, xmax, ymax), c in zip(processed['labels'][keep], processed['scores'][keep], processed['boxes'][keep], colors):
+        print(processed['labels'][keep])
+        for label, score, (xmin, ymin, xmax, ymax) in zip(processed['labels'][keep], processed['scores'][keep], processed['boxes'][keep]):
             ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
-                                       fill=False, color=c, linewidth=3))
-            text = f'Door: {score:0.2f}'
+                                       fill=False, color=COLORS[label], linewidth=3))
+            text = f'{DOOR_LABELS[int(label)]}: {score:0.2f}'
             ax.text(xmin, ymin, text, fontsize=15,
                     bbox=dict(facecolor='yellow', alpha=0.5))
 
