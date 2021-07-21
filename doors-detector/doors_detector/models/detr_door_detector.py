92ce9f760d97e904638f75e3fe6324b393e482f4
micheleantonazzi <m7242872428>
2021-07-21 12:13:03 +0200

    detr_door_detector.py: added description

--- doors-detector/doors_detector/models/detr_door_detector.py
+++ doors-detector/doors_detector/models/detr_door_detector.py
@@ -13,7 +13,7 @@ class DetrDoorDetector(nn.Module):
     This class builds a door detector starting from a detr pretrained module.
     Basically it loads a dtr module and modify its structure to recognize door.
     """
-    def __init__(self, model_name: ModelName, pretrained: bool, dataset_name: DATASET):
+    def __init__(self, model_name: ModelName, pretrained: bool, dataset_name: DATASET, description: str = ''):
         """
 
         :param model_name: the name of the detr base model
@@ -24,6 +24,7 @@ class DetrDoorDetector(nn.Module):
         self._model_name = model_name
         self.model = torch.hub.load('facebookresearch/detr', model_name, pretrained=True)
         self._dataset_name = dataset_name
+        self._description = description
 
         # Freeze the model parameters
         for param in self.model.parameters():
@@ -35,7 +36,7 @@ class DetrDoorDetector(nn.Module):
         self.model.bbox_embed = MLP(256, 256, 4, 3)
 
         if pretrained:
-            path = os.path.join(os.path.dirname(__file__), 'train_params', self._model_name, str(self._dataset_name))
+            path = os.path.join(os.path.dirname(__file__), 'train_params', self._model_name + self._description, str(self._dataset_name))
             self.model.class_embed.load_state_dict(torch.load(os.path.join(path, 'class_embed.pth')))
             self.model.bbox_embed.load_state_dict(torch.load(os.path.join(path, 'bbox_embed.pth')))
 
@@ -65,7 +66,7 @@ class DetrDoorDetector(nn.Module):
         self.model.to(device)
 
     def save(self, epoch, optimizer_state_dict, lr_scheduler_state_dict, params, logs):
-        path = os.path.join(os.path.dirname(__file__), 'train_params', self._model_name)
+        path = os.path.join(os.path.dirname(__file__), 'train_params', self._model_name + self._description)
 
         if not os.path.exists(path):
             os.mkdir(path)
