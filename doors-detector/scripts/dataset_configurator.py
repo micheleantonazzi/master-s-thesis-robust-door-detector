798a36b002f813dedd763a1609053c7d0c5f9e9f
micheleantonazzi <m7242872428>
2021-08-03 09:39:13 +0200

    dataset_configurator.py: increased test set size

--- doors-detector/scripts/dataset_configurator.py
+++ doors-detector/scripts/dataset_configurator.py
@@ -16,7 +16,7 @@ def get_my_doors_sets():
     datasets_creator = DatasetsCreatorGibson(gibson_dataset_path)
     datasets_creator.consider_samples_with_label(label=1)
     datasets_creator.consider_n_folders(1)
-    train, test = datasets_creator.creates_dataset(train_size=0.9, test_size=0.1, split_folder=False, folder_train_ratio=0.8, use_all_samples=True)
+    train, test = datasets_creator.creates_dataset(train_size=0.8, test_size=0.2, split_folder=False, folder_train_ratio=0.8, use_all_samples=True)
     labels = datasets_creator.get_labels()
 
     return train, test, labels
@@ -25,7 +25,7 @@ def get_my_doors_sets():
 def get_deep_doors_2_sets():
     dataset_creator = DatasetCreatorDeepDoors2(dataset_path=deep_doors_2_dataset_path)
 
-    train, test = dataset_creator.creates_dataset(train_size=0.9, test_size=0.1)
+    train, test = dataset_creator.creates_dataset(train_size=0.8, test_size=0.2)
     labels = dataset_creator.get_label()
 
     return train, test, labels
@@ -34,7 +34,7 @@ def get_deep_doors_2_sets():
 def get_deep_doors_2_labelled_sets():
     dataset_creator = DatasetsCreatorDeepDoors2Labelled(dataset_path=deep_doors_2_labelled_dataset_path)
     dataset_creator.consider_samples_with_label(label=1)
-    train, test = dataset_creator.creates_dataset(train_size=0.9, test_size=0.1)
+    train, test = dataset_creator.creates_dataset(train_size=0.8, test_size=0.2)
     labels = dataset_creator.get_labels()
 
     return train, test, labels
