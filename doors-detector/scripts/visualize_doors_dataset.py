9f37332457d636a9666815102ffee6d1478a319b
micheleantonazzi <m7242872428>
2021-09-03 10:33:58 +0200

    visualize_doors_dataset.py: fixed

--- doors-detector/scripts/visualize_doors_dataset.py
+++ doors-detector/scripts/visualize_doors_dataset.py
@@ -1,11 +1,16 @@
 import random
+
+import numpy as np
 import torch
 from matplotlib import pyplot as plt
 import torchvision.transforms as T
+from doors_detector.dataset.dataset_doors_final.datasets_creator_doors_final import DatasetsCreatorDoorsFinal
+from doors_detector.dataset.torch_dataset import DEEP_DOORS_2_LABELLED
 from doors_detector.models.detr import PostProcess
-
+from doors_detector.models.detr_door_detector import *
+from doors_detector.models.model_names import DETR_RESNET50
+from doors_detector.utilities.utils import seed_everything
 from scripts.dataset_configurator import *
-
 
 params = {
     'seed': 0
@@ -15,56 +20,51 @@ params = {
 if __name__ == '__main__':
 
     # Fix seeds
-    torch.manual_seed(params['seed'])
-    np.random.seed(params['seed'])
-    random.seed(params['seed'])
+    seed_everything(params['seed'])
 
     train, test, labels, COLORS = get_deep_doors_2_labelled_sets()
-    #train, test, labels, COLORS = get_final_doors_dataset()
-
-    post_processor = PostProcess()
 
     print(f'Train set size: {len(train)}', f'Test set size: {len(test)}')
 
-    for i in range(10):
-        img, target, door_sample = train[i]
+    model = DetrDoorDetector(model_name=DETR_RESNET50, n_labels=len(labels.keys()), pretrained=True, dataset_name=DEEP_DOORS_2_LABELLED, description=DEEP_DOORS_2_LABELLED_EXP)
+    model.eval()
 
-        boxes = target['boxes']
-        outputs = {'pred_boxes': boxes.unsqueeze(0),
-                   'pred_logits': torch.tensor([[[-0.99, - 0.2, -0.2, -0.2] for _ in range(len(boxes))]], dtype=torch.float32)
-                   }
+    for i in range(10, 50):
+        img, target, door_sample = test[i]
+        img = img.unsqueeze(0)
+        outputs = model(img)
 
-        for i, label in enumerate(target['labels']):
-            outputs['pred_logits'][0][i][label] = 0.7
+        """
+        # Print real boxes
+        outputs['pred_logits'] = torch.tensor([[[0, 1.0] for b in target['boxes']]], dtype=torch.float32)
+        outputs['pred_boxes'] = target['boxes'].unsqueeze(0)
+        """
 
-        processed = post_processor(outputs=outputs, target_sizes=torch.tensor([img.size()[1:]]))[0]
+        post_processor = PostProcess()
+        img_size = list(img.size()[2:])
+        processed_data = post_processor(outputs=outputs, target_sizes=torch.tensor([img_size]))
 
-        keep = processed['scores'] > 0.01
+        for image_data in processed_data:
+            # keep only predictions with 0.7+ confidence
 
-        # Show image with bboxes
+            keep = image_data['scores'] > 0.2
 
-        # Denormalize image tensor and convert to PIL image
-        pil_image = img * torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1)
-        pil_image = pil_image + torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1)
-        plt.figure(figsize=(16, 10))
+            # Show image with bboxes
 
-        img = T.ToPILImage()(pil_image)
-        plt.imshow(img)
-        ax = plt.gca()
+            # Denormalize image tensor and convert to PIL image
+            pil_image = img * torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1)
+            pil_image = pil_image + torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1)
+            plt.figure(figsize=(16, 10))
 
-        for label, score, (xmin, ymin, xmax, ymax) in zip(processed['labels'][keep], processed['scores'][keep], processed['boxes'][keep]):
-            ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
-                                       fill=False, color=COLORS[label], linewidth=3))
-            text = f'{labels[int(label)]}: {score:0.2f}'
-            ax.text(xmin, ymin, text, fontsize=15,
-                    bbox=dict(facecolor='yellow', alpha=0.5))
+            plt.imshow(T.ToPILImage()(pil_image[0]))
+            ax = plt.gca()
 
-        plt.axis('off')
-        plt.show()
+            for label, score, (xmin, ymin, xmax, ymax) in zip(image_data['labels'][keep], image_data['scores'][keep], image_data['boxes'][keep]):
+                ax.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                           fill=False, color=COLORS[label], linewidth=3))
+                text = f'{labels[int(label)]}: {score:0.2f}'
+                ax.text(xmin, ymin, text, fontsize=15,
+                        bbox=dict(facecolor='yellow', alpha=0.5))
 
-        door_sample.visualize()
-
-
-
-
-
+            plt.axis('off')
+            plt.show()
