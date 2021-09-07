7b84af2fa66a33bf98bf47078af340fa07ff0573
micheleantonazzi <micheleantonazzi@gmail.com>
2021-09-07 13:19:47 +0200

    print_logs.py: fixed

--- doors-detector/scripts/print_logs.py
+++ doors-detector/scripts/print_logs.py
@@ -1,10 +1,10 @@
-from doors_detector.dataset.torch_dataset import DEEP_DOORS_2
+from doors_detector.dataset.torch_dataset import DEEP_DOORS_2_LABELLED, FINAL_DOORS_DATASET
 from doors_detector.models.detr_door_detector import *
 from doors_detector.models.model_names import DETR_RESNET50
 from doors_detector.utilities.plot import plot_losses
 
 
-model = DetrDoorDetector(model_name=DETR_RESNET50, pretrained=True, dataset_name=DEEP_DOORS_2, description=PRETRAINED_FINETUNE_ALL_LR_LOW_NOSTEP_NOAUG_10OBJQUERIES_LABELLED)
+model = DetrDoorDetector(model_name=DETR_RESNET50, n_labels=2, pretrained=True, dataset_name=FINAL_DOORS_DATASET, description=EXP_1_HOUSE_1)
 checkpoint = model.load_checkpoint()
 
 plot_losses(checkpoint['logs'])
