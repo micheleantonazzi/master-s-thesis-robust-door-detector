928afb294f477cc177bb3b79514f4a4baff7f940
micheleantonazzi <m7242872428>
2021-11-10 16:25:01 +0100

    thesis upgrade

--- doors-detector/scripts/print_logs.py
+++ doors-detector/scripts/print_logs.py
@@ -4,7 +4,7 @@ from doors_detector.models.model_names import DETR_RESNET50
 from doors_detector.utilities.plot import plot_losses
 
 
-model = DetrDoorDetector(model_name=DETR_RESNET50, n_labels=2, pretrained=True, dataset_name=FINAL_DOORS_DATASET, description=EXP_1_HOUSE_1)
+model = DetrDoorDetector(model_name=DETR_RESNET50, n_labels=2, pretrained=True, dataset_name=FINAL_DOORS_DATASET, description=EXP_2_HOUSE_1_25)
 checkpoint = model.load_checkpoint()
 
 plot_losses(checkpoint['logs'])
