07a6b4edbd5d0e11d5c1c6448b4e19d23c9cb7f2
micheleantonazzi <micheleantonazzi@gmail.com>
2021-11-26 18:47:57 +0100

    added legend to losses plot

--- doors-detector/doors_detector/utilities/plot.py
+++ doors-detector/doors_detector/utilities/plot.py
@@ -11,12 +11,12 @@ def plot_losses(logs: Dict[str, List[Dict[str, float]]], save_to_file: bool = Fa
     losses = list(train_logs[0].keys())
 
     for loss in losses:
-        plt.title(loss)
+        plt.title(loss.capitalize())
 
         # Print train logs
-        plt.plot([i for i in range(len(train_logs))], [log_epoch[loss] for log_epoch in train_logs])
+        plt.plot([i for i in range(len(train_logs))], [log_epoch[loss] for log_epoch in train_logs], label='Train loss')
 
         # Print test logs
-        plt.plot([i for i in range(len(test_logs))], [log_epoch[loss] for log_epoch in test_logs])
-
+        plt.plot([i for i in range(len(test_logs))], [log_epoch[loss] for log_epoch in test_logs], label='Test loss')
+        plt.legend()
         plt.show()
