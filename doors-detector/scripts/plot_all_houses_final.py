6bcfe2f6398247d89ff57cb7b6e1bba9fdd200f9
micheleantonazzi <m7242872428>
2022-02-28 11:29:28 +0100

    plot_all_houses_final.py: fixed graphics

--- doors-detector/scripts/plot_all_houses_final.py
+++ doors-detector/scripts/plot_all_houses_final.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, font_manager
 from matplotlib.pyplot import subplots
 from pandas import CategoricalDtype
 
@@ -8,7 +8,7 @@ houses = pd.read_csv('./../results/risultati_tesi_antonazzi.csv')
 houses['AP'] = houses['AP'].astype(np.float64)
 houses['AP'] = houses['AP'].apply(lambda x: x*100).round()
 
-labels = ['$GD$', '$FD_{25}$', '$FD_{50}$', '$FD_{75}$']
+labels = ['$GD_e$', '$FD^{25}_e$', '$FD^{50}_e$', '$FD^{75}_e$']
 experiments = ['1', '2a', '2b', '2c']
 
 houses_list_dtype = CategoricalDtype(
@@ -28,20 +28,25 @@ open_doors = open_doors.sort_values(['Env name'])
 
 fig, ax = subplots(figsize=(10, 5))
 
-print(closed_doors[('AP', '1')].tolist())
+X = np.arange(11)
 
-X = np.arange(10)
-ax.bar(X, closed_doors[('AP', experiments[0])].tolist(), width=0.2, label=labels[0])
-ax.bar(X + 0.2, closed_doors[('AP', experiments[1])].tolist(),  width=0.2, label=labels[1])
-ax.bar(X + 0.4, closed_doors[('AP', experiments[2])].tolist(),  width=0.2, label=labels[2])
-ax.bar(X + 0.6, closed_doors[('AP', experiments[3])].tolist(),  width=0.2, label=labels[3])
+ax.bar(X, closed_doors[('AP', experiments[0])].tolist() + [closed_doors[('AP', experiments[0])].mean()], width=0.2, label=labels[0])
+ax.bar(X + 0.2, closed_doors[('AP', experiments[1])].tolist() + [closed_doors[('AP', experiments[1])].mean()],  width=0.2, label=labels[1])
+ax.bar(X + 0.4, closed_doors[('AP', experiments[2])].tolist() + [closed_doors[('AP', experiments[2])].mean()],  width=0.2, label=labels[2])
+ax.bar(X + 0.6, closed_doors[('AP', experiments[3])].tolist() + [closed_doors[('AP', experiments[3])].mean()],  width=0.2, label=labels[3])
 
+ax.set_title('AP results over all houses - closed doors (0)', fontsize=18)
 ax.set_ylim([0, 110])
-ax.set_xticks([i+0.3 for i in range(10)])
-ax.set_xticklabels(closed_doors[('Env name','')].tolist())
-ax.legend()
 
-#fig.tight_layout()
+ax.tick_params(axis='y', labelsize=16)
+ax.set_xticks([i+0.3 for i in range(11)])
+ax.set_xticklabels([f'$e_{i}$' for i in range(10)] + ['$\overline{e}$'], fontsize=17)
+ax.set_ylabel('AP', fontsize=17)
+ax.set_xlabel('Environment', fontsize=17)
+
+ax.legend(prop={"size": 16}, loc='upper center', ncol=4)
+
+fig.tight_layout()
 plt.show()
 
 plt.close()
@@ -50,16 +55,21 @@ fig, ax = subplots(figsize=(10, 5))
 
 print(closed_doors[('AP', '1')].tolist())
 
-X = np.arange(10)
-ax.bar(X, open_doors[('AP', experiments[0])].tolist(), width=0.2, label=labels[0])
-ax.bar(X + 0.2, open_doors[('AP', experiments[1])].tolist(),  width=0.2, label=labels[1])
-ax.bar(X + 0.4, open_doors[('AP', experiments[2])].tolist(),  width=0.2, label=labels[2])
-ax.bar(X + 0.6, open_doors[('AP', experiments[3])].tolist(),  width=0.2, label=labels[3])
+X = np.arange(11)
+ax.bar(X, open_doors[('AP', experiments[0])].tolist() + [open_doors[('AP', experiments[0])].mean()], width=0.2, label=labels[0])
+ax.bar(X + 0.2, open_doors[('AP', experiments[1])].tolist() + [open_doors[('AP', experiments[1])].mean()],  width=0.2, label=labels[1])
+ax.bar(X + 0.4, open_doors[('AP', experiments[2])].tolist() + [open_doors[('AP', experiments[2])].mean()],  width=0.2, label=labels[2])
+ax.bar(X + 0.6, open_doors[('AP', experiments[3])].tolist() + [open_doors[('AP', experiments[3])].mean()],  width=0.2, label=labels[3])
 
+ax.set_title('AP results over all houses - open doors (1)', fontsize=18)
 ax.set_ylim([0, 110])
-ax.set_xticks([i+0.3 for i in range(10)])
-ax.set_xticklabels(open_doors[('Env name','')].tolist())
-ax.legend()
+ax.tick_params(axis='y', labelsize=16)
+ax.set_xticks([i+0.3 for i in range(11)])
+ax.set_xticklabels([f'$e_{i}$' for i in range(10)] + ['$\overline{e}$'], fontsize=17)
+ax.set_ylabel('AP', fontsize=17)
+ax.set_xlabel('Environment', fontsize=17)
 
-#fig.tight_layout()
+ax.legend(prop={"size": 16}, loc='upper center', ncol=4)
+
+fig.tight_layout()
 plt.show()
