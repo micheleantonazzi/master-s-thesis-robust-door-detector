9cc9f3cfb40406616fdf20eb2233a3493ae978a4
micheleantonazzi <micheleantonazzi@gmail.com>
2022-02-28 17:56:45 +0100

    save_model_detections.py: fixed

--- doors-detector/scripts/save_model_detections.py
+++ doors-detector/scripts/save_model_detections.py
@@ -1,4 +1,6 @@
+import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 import torchvision.transforms as T
 import os
 from doors_detector.dataset.torch_dataset import FINAL_DOORS_DATASET
@@ -9,14 +11,7 @@ from doors_detector.utilities.utils import seed_everything
 from scripts.dataset_configurator import get_final_doors_dataset
 
 house = 'house1'
-save_path = '/home/michele/model_detections/'
 
-if not os.path.exists(save_path):
-    os.mkdir(save_path)
-
-save_path = os.path.join(save_path, house)
-if not os.path.exists(save_path):
-    os.mkdir(save_path)
 
 
 params = {
@@ -38,45 +33,69 @@ houses = {
     'house22': [EXP_1_HOUSE_22, EXP_2_HOUSE_22_25, EXP_2_HOUSE_22_50, EXP_2_HOUSE_22_75],
 }
 
-train, test, labels, COLORS = get_final_doors_dataset(2, house, train_size=0.25, use_negatives=True)
-models = [
-    (DetrDoorDetector(model_name=DETR_RESNET50, n_labels=len(labels.keys()), pretrained=True, dataset_name=FINAL_DOORS_DATASET, description=model), title)
-    for model, title in zip(houses[house],  ['GD', 'QD25', 'QD50', 'QD75'])
-]
+for house in houses.keys():
+    save_path = '/media/antonazzi/hdd/model_detections/'
 
-for i, (img, target, door_sample) in enumerate(test):
-    if len(target['boxes']) == 0:
-        continue
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
 
-    img = img.unsqueeze(0)
-    outputs = [(model(img), title) for model, title in models]
+    save_path = os.path.join(save_path, house)
+    if not os.path.exists(save_path):
+        os.mkdir(save_path)
+    train, test, labels, COLORS = get_final_doors_dataset(2, house, train_size=0.25, use_negatives=True)
+    models = [
+        (DetrDoorDetector(model_name=DETR_RESNET50, n_labels=len(labels.keys()), pretrained=True, dataset_name=FINAL_DOORS_DATASET, description=model), title)
+        for model, title in zip(houses[house],  ['GD', 'QD25', 'QD50', 'QD75'])
+    ]
 
-    post_processor = PostProcess()
-    img_size = list(img.size()[2:])
-    processed_data_models = [(post_processor(outputs=output, target_sizes=torch.tensor([img_size])), title) for output, title in outputs]
+    for i, (img, target, door_sample) in enumerate(test):
+        if len(target['boxes']) == 0:
+            continue
 
-    pil_image = img * torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1)
-    pil_image = pil_image + torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1)
-    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 10))
+        img = img.unsqueeze(0)
+        outputs = [(model(img), title) for model, title in models]
 
-    for axis, post_processed_data in zip(axes.flatten(), processed_data_models):
+        post_processor = PostProcess()
+        img_size = list(img.size()[2:])
+        processed_data_models = [(post_processor(outputs=output, target_sizes=torch.tensor([img_size])), title) for output, title in outputs]
 
-        axis.imshow(T.ToPILImage()(pil_image[0]))
+        #pil_image = img * torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1)
+        #pil_image = pil_image + torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1)
+        #fig, axes = plt.subplots(nrows=2, ncols=2)
 
-        image_data, title = post_processed_data[0][0], post_processed_data[1]
-        keep = image_data['scores'] > 0.5
-        for label, score, (xmin, ymin, xmax, ymax) in zip(image_data['labels'][keep], image_data['scores'][keep], image_data['boxes'][keep]):
-            label = label.item()
-            axis.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
-                                       fill=False, color=COLORS[label], linewidth=3))
-            text = f'{labels[int(label)]}: {score:0.2f}'
-            axis.text(xmin, ymin, text, fontsize=12,
-                    bbox=dict(facecolor='yellow', alpha=0.5))
-            #axis.set_title(title)
+        imgs = [door_sample.get_bgr_image().copy() for _ in range(4)]
+        for img, post_processed_data in zip(imgs, processed_data_models):
+            image_data, title = post_processed_data[0][0], post_processed_data[1]
+            keep = image_data['scores'] > 0.5
+            for label, score, (xmin, ymin, xmax, ymax) in zip(image_data['labels'][keep], image_data['scores'][keep], image_data['boxes'][keep]):
+                label = label.item()
 
-        axis.axis('off')
+                (xmin, ymin, xmax, ymax) = np.rint(np.array([xmin, ymin, xmax, ymax])).astype('int32')
+                img = cv2.rectangle(img, (xmin, ymin), (xmax, ymax), color=(np.array(COLORS[label])*255)[::-1].astype('int16').tolist(), thickness=2)
+        r1 = np.concatenate((imgs[0], imgs[1]), axis=0)
+        r2 = np.concatenate((imgs[2], imgs[3]), axis=0)
+        final_image = np.concatenate((r1, r2), axis=1)
+        #cv2.imwrite(save_path + f'/{i}.pdf', final_image)
+        cv2.imwrite(save_path + f'/{i}.png', final_image)
+        """for axis, post_processed_data in zip(axes.flatten(), processed_data_models):
 
-    fig.savefig(save_path + f'/{i}.png')
+            axis.imshow(T.ToPILImage()(pil_image[0]),cmap='',interpolation='')
+
+            image_data, title = post_processed_data[0][0], post_processed_data[1]
+            keep = image_data['scores'] > 0.5
+            for label, score, (xmin, ymin, xmax, ymax) in zip(image_data['labels'][keep], image_data['scores'][keep], image_data['boxes'][keep]):
+                label = label.item()
+                axis.add_patch(plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin,
+                                           fill=False, color=COLORS[label], linewidth=1))
+                text = f'{labels[int(label)]}: {score:0.2f}'
+                #axis.text(xmin, ymin - 2, text, fontsize=5)
+                #axis.set_title(title)
+
+            axis.axis('off')
+        
+        fig.savefig(save_path + f'/{i}.pdf')
+        fig.savefig(save_path + f'/{i}.png')
+        """
 
 
 
